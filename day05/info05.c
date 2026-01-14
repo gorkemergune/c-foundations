0757b966c86e4273ed02bdafe5c14307002d8242
Görkem Paradise <smailgorkem@gmail.com>
2026-01-14 15:25:38 +0300

    Day5

--- day05/info05.c
+++ day05/info05.c
@@ -4,3 +4,7 @@ int main() {
     printf("Day 5 started.\n");
     return 0;
 }
+
+// Today I studied C Pointers, including:
+// Memory Address, Pointers & Arrays,
+// Pointer Arithmetic, Pointer to Pointer
