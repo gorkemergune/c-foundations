b113058426eb4b7f6cd92241549a53e4e965f3b9
Görkem Paradise <smailgorkem@gmail.com>
2026-01-17 01:53:06 +0300

    Day7

--- day07/info07.c
+++ day07/info07.c
@@ -4,3 +4,9 @@ int main() {
     printf("Day 7 started.\n");
     return 0;
 }
+
+// Today I studied functions in C:
+// Function declaration vs definition
+// Function parameters and arguments
+// Return values from functions
+// Function scope and global scope
